50e7d11c64f2a102286160f824cf54fdb7f2f363
Dijin <dijin@lookup.to>
2016-04-29 12:16:43 +0530

    key and values

---
+++ js/_script8.js
@@ -0,0 +1,8 @@
+// objects
+
+var Tuts = { NetTuts : 'Web Development',  WPTuts : 'WordPress',  PSDTuts : 'PhotoShop', AeTuts : 'After Effects'};
+
+var keys = _.keys(Tuts);
+var value = _.values(Tuts);
+
+console.log(keys + value);
