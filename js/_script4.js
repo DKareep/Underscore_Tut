937e9e4a4ef7c19c8047a0c0afe5aa54f8920687
Dijin <dijin@lookup.to>
2016-04-29 12:06:40 +0530

    uniq feature

--- js/_script4.js
+++ js/_script4.js
@@ -2,4 +2,4 @@ var scores = [34, 11, 56, 93, 99, 89, 81, 33, 40];
 var isGreaterThanTen = _(scores).all(function(score){ return score > 10});
 console.log(isGreaterThanTen);
 
-// checks all the elements in an array
+// checks all the elements in an collection
---
+++ js/_script5.js
@@ -0,0 +1,7 @@
+// array
+var scores = [34, 11, 56, 93, 99, 89, 81, 33, 34, 40, 12, 11, 99];
+
+var uniqueNumber = _.uniq(scores);
+console.log(uniqueNumber);
+
+// ignores duplicated entries and returns array
